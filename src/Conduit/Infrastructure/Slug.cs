de18c9fad1fca5776df11d2ddae2c9c55ee77c33
hosamelgendy100 <hosamelgendy100@yahoo.com>
2020-02-10 18:38:02 +0300

    first commit

---
+++ src/Conduit/Infrastructure/Slug.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Conduit.Infrastructure
+{
+    public static class Slug
+    {
+        public static string GenerateSlug(this string phrase)
+        {
+            IdnMapping idn = new IdnMapping();
+            string punyCode = idn.GetAscii(phrase);
+            return punyCode;
+        }
+
+        public static string RemoveDiacritics(this string text)
+        {
+            var s = new string(text.Normalize(NormalizationForm.FormD)
+                .Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+                .ToArray());
+
+            return s.Normalize(NormalizationForm.FormC);
+        }
+    }
+}
